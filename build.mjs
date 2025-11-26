34ffb1df83f5fdde01533050e41cab5246c9f3ef
sahTitus <sahtitus58@gmail.com>
2025-11-26 16:49:58 +0000

    renamed server.ts to index.ts

--- build.mjs
+++ build.mjs
@@ -1,7 +1,7 @@
 import esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["src/server.ts"],
+  entryPoints: ["src/index.ts"],
   bundle: true,
   platform: "node",
   target: "node20",
