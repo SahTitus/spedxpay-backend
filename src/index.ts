c1cb1180ca309623ccd8e881e30235d65efeabad
sahTitus <sahtitus58@gmail.com>
2025-11-27 10:02:10 +0000

    feat: updated vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createApp } from "./app"
+import { createApp } from "./app/index"
 import { connectDatabase } from "./config/database.config"
 import { appConfig } from "./config/app.config"
 import { logger } from "./utils/logger"
